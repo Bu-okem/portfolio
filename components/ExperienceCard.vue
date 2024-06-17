80347da0c2f9b25f47c19836fce761c5bb1e1d75
bu-okem <chiobiegbulem@gmail.com>
2024-06-18 00:53:00 +0100

    feat: experience page

---
+++ components/ExperienceCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative flex justify-between gap-x-[60px] max-w-[495px]">
+    <h5
+      class="font-light font-ubuntu text-[20px] absolute bottom-0 right-0 lg:relative">
+      2023 - present
+    </h5>
+    <div class="">
+      <h4
+        class="font-ubuntu-condensed capitalize text-[25px] mb-[22px] leading-[normal]">
+        frontend web developer
+      </h4>
+      <h5 class="font-light font-ubuntu capitalize text-[20px]">
+        stock afrika
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
--- pages/experience.vue
+++ pages/experience.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="">
-    <p class="">experience</p>
+  <div class="flex flex-col gap-y-[45px] lg:gap-y-[60px] lg:h-fit lg:mb-[40px]">
+    <ExperienceCard />
+    <ExperienceCard />
+    <ExperienceCard />
+    <ExperienceCard />
+    <ExperienceCard />
   </div>
 </template>
 
