feb81d5c405cbe89d305cecc9390fbcd02405b03
bu-okem <chiobiegbulem@gmail.com>
2024-07-21 03:49:56 +0100

    feat: resume page and layout

---
+++ pages/resume.vue
@@ -0,0 +1,11 @@
+<template>
+  <iframe :src="Resume" width="100%" height="655px"> </iframe>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'pdf',
+});
+
+import Resume from '@/assets/resume.pdf';
+</script>
